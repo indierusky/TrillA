0128c5f67d666fc19a0cfccef712079ef36f885f
App Academy Student <email@email.com>
2020-11-17 09:51:36 -0500

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class_name: 'Board'
 
 
 def self.find_by_credentials(email, password) 
-    user = User.find_by(:email email) 
+    user = User.find_by(email: email) 
 
     return nil unless user 
 
