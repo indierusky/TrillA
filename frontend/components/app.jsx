2fec95342171ab556d21342b3af4cb93958fc1ec
App Academy Student <email@email.com>
2020-11-17 21:49:04 -0500

    updates

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import SignupContainer from './session/signup_container';
 import SigninContainer from './session/signin_container';
 import NavContainer from './navigation/nav_container';
-import Home from './home/home';
 import { Route } from 'react-router-dom';
 import {AuthRoute, ProtectedRoute} from '../utils/route_utils';
 
@@ -11,7 +10,6 @@ import {AuthRoute, ProtectedRoute} from '../utils/route_utils';
 export default () => (
     <div>
         <Route path="/" component={NavContainer} />
-        <Route exact path="/" component={Home} />
         <AuthRoute path="/signup" component={SignupContainer} />
         <AuthRoute path="/login" component={SigninContainer} />
     </div>
