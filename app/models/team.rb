ca84f1ade55eb4f5857c5c6724ac17f0da58ce71
App Academy Student <email@email.com>
2020-11-29 16:30:08 -0500

    updates

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,16 +1,20 @@
 class Team < ApplicationRecord
 
-validates :title,  presence: true
-validates :board_id, :presence => true 
+validates :title,  :presence => true
+validates :board_id, :presence => true, :uniqueness => true
 validates :group_id, :presence => true	
    #avoid in memory duplicates. Also added unique index to db
+
+
 validates :user_id, :uniqueness => {:scope => [:user_id, :board_id]}	
 
 
-end
+
 
 belongs_to :board 
 belongs_to :user 
+
+
 
 
 
